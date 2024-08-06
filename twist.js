ff32a3a363afe1d1fd5f9256fd9bb632109c78e9
Victor Benedict <victorbenedict.bulaong@gmail.com>
2024-08-06 15:43:15 +0800

    Submit kata

---
+++ twist.js
@@ -0,0 +1,7 @@
+/**
+ * Kata - https://www.codewars.com/kata/525c1a07bb6dda6944000031
+ * add the value "codewars" to the websites array 1,000 times
+ *
+ */
+
+const websites = new Array(1000).fill('codewars');
