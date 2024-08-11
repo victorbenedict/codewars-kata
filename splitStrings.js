d8450c2ed7c12b71598b173e1e6d6e215f41be6f
Victor Benedict <victorbenedict.bulaong@gmail.com>
2024-08-12 00:02:19 +0800

    Submit kata

---
+++ splitStrings.js
@@ -0,0 +1,12 @@
+/**
+ * Kata - https://www.codewars.com/kata/515de9ae9dcfc28eb6000001
+ *
+ * Splits the string into pairs of two characters
+ * @param {string} str - The input string.
+ * @returns {[]string} - Returns an array with pairs of strings. If the pair contains odd number the second character is replaced with underscore.
+ */
+function solution(str) {
+  if (str.length === 0) return [];
+  if (str.length % 2 === 1) str += '_';
+  return str.match(/.{1,2}/g);
+}
