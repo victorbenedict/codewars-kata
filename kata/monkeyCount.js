e2768a085636a6f67a3c1e1577df1b75d54d9ebb
Victor Benedict <victorbenedict.bulaong@gmail.com>
2024-09-03 23:46:41 +0800

    Submit kata

---
+++ kata/monkeyCount.js
@@ -0,0 +1,324 @@
+/**
+ * Kata - https://www.codewars.com/kata/5bb904724c47249b10000131
+ *
+ * Convert the total number to an array of numbers
+ * @param {number} games Total number of monkeys.
+ * @returns {number[]}
+ */
+function ifFunction(n) {
+  for (let index = 1; index <= n; index++) {
+    console.log(`if (n + 1 > ${index}) {
+      result.push(${index})
+    }`);
+    // your code here
+  }
+}
+
+function monkeyCount(n) {
+  let result = [];
+
+  if (n + 1 > 1) {
+    result.push(1);
+  }
+  if (n + 1 > 2) {
+    result.push(2);
+  }
+  if (n + 1 > 3) {
+    result.push(3);
+  }
+  if (n + 1 > 4) {
+    result.push(4);
+  }
+  if (n + 1 > 5) {
+    result.push(5);
+  }
+  if (n + 1 > 6) {
+    result.push(6);
+  }
+  if (n + 1 > 7) {
+    result.push(7);
+  }
+  if (n + 1 > 8) {
+    result.push(8);
+  }
+  if (n + 1 > 9) {
+    result.push(9);
+  }
+  if (n + 1 > 10) {
+    result.push(10);
+  }
+  if (n + 1 > 11) {
+    result.push(11);
+  }
+  if (n + 1 > 12) {
+    result.push(12);
+  }
+  if (n + 1 > 13) {
+    result.push(13);
+  }
+  if (n + 1 > 14) {
+    result.push(14);
+  }
+  if (n + 1 > 15) {
+    result.push(15);
+  }
+  if (n + 1 > 16) {
+    result.push(16);
+  }
+  if (n + 1 > 17) {
+    result.push(17);
+  }
+  if (n + 1 > 18) {
+    result.push(18);
+  }
+  if (n + 1 > 19) {
+    result.push(19);
+  }
+  if (n + 1 > 20) {
+    result.push(20);
+  }
+  if (n + 1 > 21) {
+    result.push(21);
+  }
+  if (n + 1 > 22) {
+    result.push(22);
+  }
+  if (n + 1 > 23) {
+    result.push(23);
+  }
+  if (n + 1 > 24) {
+    result.push(24);
+  }
+  if (n + 1 > 25) {
+    result.push(25);
+  }
+  if (n + 1 > 26) {
+    result.push(26);
+  }
+  if (n + 1 > 27) {
+    result.push(27);
+  }
+  if (n + 1 > 28) {
+    result.push(28);
+  }
+  if (n + 1 > 29) {
+    result.push(29);
+  }
+  if (n + 1 > 30) {
+    result.push(30);
+  }
+  if (n + 1 > 31) {
+    result.push(31);
+  }
+  if (n + 1 > 32) {
+    result.push(32);
+  }
+  if (n + 1 > 33) {
+    result.push(33);
+  }
+  if (n + 1 > 34) {
+    result.push(34);
+  }
+  if (n + 1 > 35) {
+    result.push(35);
+  }
+  if (n + 1 > 36) {
+    result.push(36);
+  }
+  if (n + 1 > 37) {
+    result.push(37);
+  }
+  if (n + 1 > 38) {
+    result.push(38);
+  }
+  if (n + 1 > 39) {
+    result.push(39);
+  }
+  if (n + 1 > 40) {
+    result.push(40);
+  }
+  if (n + 1 > 41) {
+    result.push(41);
+  }
+  if (n + 1 > 42) {
+    result.push(42);
+  }
+  if (n + 1 > 43) {
+    result.push(43);
+  }
+  if (n + 1 > 44) {
+    result.push(44);
+  }
+  if (n + 1 > 45) {
+    result.push(45);
+  }
+  if (n + 1 > 46) {
+    result.push(46);
+  }
+  if (n + 1 > 47) {
+    result.push(47);
+  }
+  if (n + 1 > 48) {
+    result.push(48);
+  }
+  if (n + 1 > 49) {
+    result.push(49);
+  }
+  if (n + 1 > 50) {
+    result.push(50);
+  }
+  if (n + 1 > 51) {
+    result.push(51);
+  }
+  if (n + 1 > 52) {
+    result.push(52);
+  }
+  if (n + 1 > 53) {
+    result.push(53);
+  }
+  if (n + 1 > 54) {
+    result.push(54);
+  }
+  if (n + 1 > 55) {
+    result.push(55);
+  }
+  if (n + 1 > 56) {
+    result.push(56);
+  }
+  if (n + 1 > 57) {
+    result.push(57);
+  }
+  if (n + 1 > 58) {
+    result.push(58);
+  }
+  if (n + 1 > 59) {
+    result.push(59);
+  }
+  if (n + 1 > 60) {
+    result.push(60);
+  }
+  if (n + 1 > 61) {
+    result.push(61);
+  }
+  if (n + 1 > 62) {
+    result.push(62);
+  }
+  if (n + 1 > 63) {
+    result.push(63);
+  }
+  if (n + 1 > 64) {
+    result.push(64);
+  }
+  if (n + 1 > 65) {
+    result.push(65);
+  }
+  if (n + 1 > 66) {
+    result.push(66);
+  }
+  if (n + 1 > 67) {
+    result.push(67);
+  }
+  if (n + 1 > 68) {
+    result.push(68);
+  }
+  if (n + 1 > 69) {
+    result.push(69);
+  }
+  if (n + 1 > 70) {
+    result.push(70);
+  }
+  if (n + 1 > 71) {
+    result.push(71);
+  }
+  if (n + 1 > 72) {
+    result.push(72);
+  }
+  if (n + 1 > 73) {
+    result.push(73);
+  }
+  if (n + 1 > 74) {
+    result.push(74);
+  }
+  if (n + 1 > 75) {
+    result.push(75);
+  }
+  if (n + 1 > 76) {
+    result.push(76);
+  }
+  if (n + 1 > 77) {
+    result.push(77);
+  }
+  if (n + 1 > 78) {
+    result.push(78);
+  }
+  if (n + 1 > 79) {
+    result.push(79);
+  }
+  if (n + 1 > 80) {
+    result.push(80);
+  }
+  if (n + 1 > 81) {
+    result.push(81);
+  }
+  if (n + 1 > 82) {
+    result.push(82);
+  }
+  if (n + 1 > 83) {
+    result.push(83);
+  }
+  if (n + 1 > 84) {
+    result.push(84);
+  }
+  if (n + 1 > 85) {
+    result.push(85);
+  }
+  if (n + 1 > 86) {
+    result.push(86);
+  }
+  if (n + 1 > 87) {
+    result.push(87);
+  }
+  if (n + 1 > 88) {
+    result.push(88);
+  }
+  if (n + 1 > 89) {
+    result.push(89);
+  }
+  if (n + 1 > 90) {
+    result.push(90);
+  }
+  if (n + 1 > 91) {
+    result.push(91);
+  }
+  if (n + 1 > 92) {
+    result.push(92);
+  }
+  if (n + 1 > 93) {
+    result.push(93);
+  }
+  if (n + 1 > 94) {
+    result.push(94);
+  }
+  if (n + 1 > 95) {
+    result.push(95);
+  }
+  if (n + 1 > 96) {
+    result.push(96);
+  }
+  if (n + 1 > 97) {
+    result.push(97);
+  }
+  if (n + 1 > 98) {
+    result.push(98);
+  }
+  if (n + 1 > 99) {
+    result.push(99);
+  }
+  if (n + 1 > 100) {
+    result.push(100);
+  }
+
+  return result;
+}
+console.log(ifFunction(100));
+console.log(monkeyCount(5));
